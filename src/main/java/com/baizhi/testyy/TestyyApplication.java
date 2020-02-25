d74d892d8faedb20ba500a4764041708e18da353
hann <dbf3ef@163.com>
2020-02-25 08:36:25 +0800

    测试IDEA第一次上传

---
+++ src/main/java/com/baizhi/testyy/TestyyApplication.java
@@ -0,0 +1,13 @@
+package com.baizhi.testyy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestyyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestyyApplication.class, args);
+    }
+
+}
